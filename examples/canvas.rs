0b0f786755a63c8589fb406d4017e5d823988463
Chase Ruskin <c.ruskin@ufl.edu>
2022-12-26 19:14:48 -0500

    tweaks example code

--- examples/canvas.rs
+++ examples/canvas.rs
@@ -1,4 +1,4 @@
-use crayon::{AsAnsi, Color, ColoredString};
+use crayon::{Color, ColoredString};
 
 fn main() -> () {
     let mut c = "blue".blue().bg_white();
@@ -7,8 +7,6 @@ fn main() -> () {
     let e = ColoredString::new("hello world");
 
     let e = e.blue();
-    let e = e.red();
-
     let text = "hello world";
 
     let s = text.red();
@@ -16,26 +14,23 @@ fn main() -> () {
     println!("{}", s.green());
 
     let d = "blue".red().bg_green();
-    println!("{:?}", c.get_code());
     println!("c: {}", c);
     println!("d: {}", d);
     println!("e: {}", e);
-    println!("{:?}", d.get_code());
-    println!("{:?}", e.get_code());
-    assert_eq!(d.get_code(), c.get_code());
 
     crayon::disable_color();
     let c = "coloring".green().bold().underline();
     println!("{} | size: {}", c, c.to_string().len());
     crayon::enable_color();
     println!("{} | size: {}", c, c.to_string().len());
+
     println!(
         "{} {} {}",
-        "red".red().bold().underline(),
+        "red".red().bold(),
         "green".green().underline(),
-        "blue".white().blue(),
+        "blue".blue(),
     );
-    let c = "go gators".green().magenta();
-    crayon::enable_color();
-    println!("{} ... {}", "go gators".yellow(), c);
+
+    let message = "go".green().to_string() + &" gators".blue().to_string();
+    println!("{} ... {}", "go gators".cyan().bold(), message);
 }
